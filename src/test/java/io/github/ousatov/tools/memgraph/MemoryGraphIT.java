783566145024f3f414c5cae2c911953ab1202b09
Oleksii Usatov <oleksii.usatov@proton.me>
2026-04-30 12:23:18 +0300

    fix: Rebased

--- src/test/java/io/github/ousatov/tools/memgraph/MemoryGraphIT.java
+++ src/test/java/io/github/ousatov/tools/memgraph/MemoryGraphIT.java
@@ -96,7 +96,7 @@ class MemoryGraphIT {
 
   @Test
   void memoryItemsCanReferToCodeRefsResolvedToCodeNodes() {
-    writer.upsertFile(TEST_FILE);
+    writer.executeWrite(tx -> writer.upsertFile(tx, TEST_FILE));
 
     session
         .run(
@@ -169,7 +169,7 @@ class MemoryGraphIT {
 
   @Test
   void wipeMemoriesDeletesProjectMemoryGraphAndPreservesCodeGraph() {
-    writer.upsertFile(TEST_FILE);
+    writer.executeWrite(tx -> writer.upsertFile(tx, TEST_FILE));
     session
         .run(
             "MATCH (m:Memory {project: $p})"
