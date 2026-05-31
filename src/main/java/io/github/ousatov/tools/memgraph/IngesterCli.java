5f73f081542a55425a040f39d317bdff81efaaec
Oleksii Usatov <oleksii.usatov@proton.me>
2026-05-31 21:43:29 +0300

    build: Bump version to 12.0.29 in CLI and pom.xml

--- src/main/java/io/github/ousatov/tools/memgraph/IngesterCli.java
+++ src/main/java/io/github/ousatov/tools/memgraph/IngesterCli.java
@@ -45,7 +45,7 @@ import picocli.CommandLine.Spec;
  *
  * @author Oleksii Usatov
  */
-@Command(name = "ingest", mixinStandardHelpOptions = true, version = "12.0.28")
+@Command(name = "ingest", mixinStandardHelpOptions = true, version = "12.0.29")
 public final class IngesterCli implements Callable<Integer> {
 
   private static final Logger log = LoggerFactory.getLogger(IngesterCli.class);
