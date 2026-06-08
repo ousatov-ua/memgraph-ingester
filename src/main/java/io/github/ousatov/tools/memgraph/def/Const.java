9742bca0a7c6bee45f52013d9b86daee22d8fdbf
Oleksii Usatov <oleksii.usatov@proton.me>
2026-06-08 20:35:43 +0300

    Release 12.2.7

--- src/main/java/io/github/ousatov/tools/memgraph/def/Const.java
+++ src/main/java/io/github/ousatov/tools/memgraph/def/Const.java
@@ -404,7 +404,7 @@ public final class Const {
   /** Command line option names and aliases. */
   public static class Cli {
 
-    public static final String VERSION = "12.2.6";
+    public static final String VERSION = "12.2.7";
     public static final String USER_SHORT = "-u";
     public static final String USER = "--user";
     public static final String PASS_SHORT = "-p";
