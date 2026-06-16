a81623b8247f29396318ed201a6b4bf1410c9ca0
Oleksii Usatov <oleksii.usatov@proton.me>
2026-06-16 15:34:26 +0300

    Bump version to 12.4.8 and update CLI version constant

--- src/main/java/io/github/ousatov/tools/memgraph/def/Const.java
+++ src/main/java/io/github/ousatov/tools/memgraph/def/Const.java
@@ -409,7 +409,7 @@ public final class Const {
   /** Command line option names and aliases. */
   public static class Cli {
 
-    public static final String VERSION = "12.4.7";
+    public static final String VERSION = "12.4.8";
     public static final String USER_SHORT = "-u";
     public static final String USER = "--user";
     public static final String PASS_SHORT = "-p";
